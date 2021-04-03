c116a895c65b199fb1cac66bf100c9dd15c4194f
Christian Mazariegos <christianmaza97@gmail.com>
2021-04-03 13:48:46 -0400

    created a front-end for the homepage with flask route for the infinity scroll

---
+++ front-end/app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/posts", methods=["GET"])
+def posts():
+
+    # get start and endpoints for the newly generated posts
+    start = int(request.args.get("start") or 0)
+    end = int(request.args.get("end") or (start + 14))
+
+    data = []
+
+    # render new cards
+    for i in range(start, end + 1):
+        data.append(render_template("card.html"))
+
+    # create an artificial delay
+    return jsonify(data)
---
+++ front-end/static/js/main.js
@@ -0,0 +1,36 @@
+let counter = 1;
+const quantity = 15;
+let hello = "hello";
+
+document.addEventListener('DOMContentLoaded', load());
+
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        load();
+    }
+});
+
+function load() {
+    const start = counter;
+    const end = start + quantity - 1;
+    counter = end + 1;
+
+    //get data for post ranging from start to end
+    fetch(`/posts?start=${start}&end=${end}`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(addPost);
+        });
+}
+
+//add new post to the DOM
+function addPost(contents) {
+    //create li Element
+    const post = document.createElement("li");
+
+    //add the class name "homeCardWrapper" and contents to the newly created post
+    //post.className = "homeCardWrapper";
+    post.innerHTML = contents;
+
+    document.querySelector("#posts").append(post);
+}
